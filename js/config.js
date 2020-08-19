e147bce4a7065649fc3a0bd6b31075cc143af52c
mikewclee <mikewclee@gmail.com>
2020-08-19 16:35:19 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 var config ={
-    yelpBearer : "Bearer BdJJtqLJAbXg46LvAvsUxVIFGDcTm1OLpdNFVi3c47LfyPltqjhffRyvyUCd0IPXhZuG3N2KOrn0faY1QG0lK412AkBNE9zFsAhYc1MgntcV7V0_veyQD9Aytqo0X3Yx"
+    yelpBearer : "API_KEY"
 } 
